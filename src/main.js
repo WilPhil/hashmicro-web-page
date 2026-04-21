a15c442a8362c7fd4e355c4c2f5afc68598ffc78
Wilsent Philip <wphilip465@gmail.com>
2026-04-21 20:13:02 +0500

    fix: preline not initialized in prod

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 import './style.css';
 import 'preline';
+
+// Preline data-hs-collapsed navbar elements
+window.addEventListener('load', () => {
+  if (window.HSStaticMethods) {
+    window.HSStaticMethods.autoInit();
+  }
+})
